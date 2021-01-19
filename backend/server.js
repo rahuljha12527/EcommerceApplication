aec1f107780c99e2e58af086e184f743caf23c0b
rahuljha661 <rahuljha12527@gmail.com>
2021-01-20 01:09:39 +0530

    add dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
-const express=require('express');
-const products=require('./data/products');
+const express = require("express");
+const dotenv = require("dotenv");
+const products = require("./data/products");
 
-const app=express();
+dotenv.config();
 
-app.get('/',(req,res)=>{
-    res.send('API is running....');
-})
+const app = express();
 
-app.get('/api/products',(req,res)=>{
-   res.json(products)    
-})
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find(p=>p._id===req.params.id)
-    res.json(product)    
- })
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000,console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
 
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port 5000 ${PORT}`
+  )
+);
