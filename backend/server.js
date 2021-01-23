c9ffca64cf80d6d6efdf3a7b6a694139d882ef2c
rahuljha661 <rahuljha12527@gmail.com>
2021-01-23 15:30:01 +0530

    add users and sample data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import  dotenv  from "dotenv";
+import colors from 'colors';
+import connectDB from './config/db.js'
+import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB()
 
 const app = express();
 
@@ -21,9 +25,11 @@ app.get("/api/products/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+console.log("porthhhhhhhhhhhhhhh",PORT);
+
 app.listen(
-  PORT,
+  5000,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port 5000 ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port 5000 ${PORT}`.yellow.bold
   )
 );
